09be7b7e06fbfe5a3301d6ee375f54a0a4cd2079
mglangel4 <mig.urrutia@duocuc.cl>
2026-05-17 01:56:05 -0400

    Repositorio de admision y paquete creados

---
+++ src/main/java/com/sivebo/ms_admision/repository/PaqueteRepository.java
@@ -0,0 +1,7 @@
+package com.sivebo.ms_admision.repository;
+
+import com.sivebo.ms_admision.model.Paquete;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaqueteRepository extends JpaRepository<Paquete, Long> {
+}
