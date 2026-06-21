b88b3774b21e2f5053465a0145183cbb433fb233
daemunozr <dae.munozr@duocuc.cl>
2026-06-21 14:52:26 -0400

    arreglado endpoints y configuracion de yml

--- src/main/java/com/sivebo/ms_admision/config/AppConfig.java
+++ src/main/java/com/sivebo/ms_admision/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.sivebo.ms_admision.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,40 +8,29 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
-        @Value("${ms.clientes.url}")
-        private String clientesBaseUrl;
-
-        @Value("${ms.sucursales.url}")
-        private String sucursalesBaseUrl;
-
-        @Value("${ms.auth.url}")
-        private String authBaseUrl;
-
-        @Value("${ms.tracking.url}")
-        private String trackingBaseUrl;
-
         @Bean
+        @LoadBalanced
         public WebClient.Builder webClientBuilder() {
                 return WebClient.builder();
         }
 
         @Bean
         public WebClient clientesWebClient(WebClient.Builder builder) {
-                return builder.baseUrl(clientesBaseUrl).build();
+                return builder.baseUrl("http://ms-clientes").build();
         }
 
         @Bean
         public WebClient sucursalesWebClient(WebClient.Builder builder) {
-                return builder.baseUrl(sucursalesBaseUrl).build();
+                return builder.baseUrl("http://ms-sucursales").build();
         }
 
         @Bean
         public WebClient authWebClient(WebClient.Builder builder) {
-                return builder.baseUrl(authBaseUrl).build();
+                return builder.baseUrl("http://ms-auth").build();
         }
 
         @Bean
         public WebClient trackingWebClient(WebClient.Builder builder) {
-                return builder.baseUrl(trackingBaseUrl).build();
+                return builder.baseUrl("http://ms-tracking").build();
         }
 }
